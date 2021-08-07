08b52d04cdd6d74963f5b90b89b8bf673fd564bd
Rishit Bhojak <51954932+rishitbhojak@users.noreply.github.com>
2021-08-07 20:33:08 +0530

    .Net Program to calculate percentage

---
+++ Overloading-Overriding-cons-des/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.txtNum3 = New System.Windows.Forms.TextBox()
+        Me.txtNum4 = New System.Windows.Forms.TextBox()
+        Me.txtSum = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtPer = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox1.Controls.Add(Me.txtNum4)
+        Me.GroupBox1.Controls.Add(Me.txtNum3)
+        Me.GroupBox1.Controls.Add(Me.txtNum2)
+        Me.GroupBox1.Controls.Add(Me.txtNum1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(330, 234)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Subjects"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox2.Controls.Add(Me.btnShow)
+        Me.GroupBox2.Controls.Add(Me.btnClear)
+        Me.GroupBox2.Controls.Add(Me.btnExit)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.txtPer)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtSum)
+        Me.GroupBox2.Location = New System.Drawing.Point(2, 252)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(330, 181)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Result"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Python"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(126, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Machine Learning"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = ".Net"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Cybersecurity"
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(161, 42)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(125, 27)
+        Me.txtNum1.TabIndex = 4
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(161, 81)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(125, 27)
+        Me.txtNum2.TabIndex = 5
+        '
+        'txtNum3
+        '
+        Me.txtNum3.Location = New System.Drawing.Point(161, 120)
+        Me.txtNum3.Name = "txtNum3"
+        Me.txtNum3.Size = New System.Drawing.Size(125, 27)
+        Me.txtNum3.TabIndex = 6
+        '
+        'txtNum4
+        '
+        Me.txtNum4.Location = New System.Drawing.Point(161, 159)
+        Me.txtNum4.Name = "txtNum4"
+        Me.txtNum4.Size = New System.Drawing.Size(125, 27)
+        Me.txtNum4.TabIndex = 7
+        '
+        'txtSum
+        '
+        Me.txtSum.Location = New System.Drawing.Point(158, 25)
+        Me.txtSum.Name = "txtSum"
+        Me.txtSum.Size = New System.Drawing.Size(125, 27)
+        Me.txtSum.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 32)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 20)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Total"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 85)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(82, 20)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Percentage"
+        '
+        'txtPer
+        '
+        Me.txtPer.Location = New System.Drawing.Point(158, 82)
+        Me.txtPer.Name = "txtPer"
+        Me.txtPer.Size = New System.Drawing.Size(125, 27)
+        Me.txtPer.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(289, 85)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(21, 20)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "%"
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.SlateGray
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnExit.Location = New System.Drawing.Point(216, 146)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 29)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(116, 146)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(94, 29)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(10, 146)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(94, 29)
+        Me.btnShow.TabIndex = 6
+        Me.btnShow.Text = "Calculate"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(331, 430)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtNum4 As TextBox
+    Friend WithEvents txtNum3 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtPer As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtSum As TextBox
+    Friend WithEvents btnShow As Button
+End Class
---
+++ Overloading-Overriding-cons-des/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNum1.Clear()
+        txtNum2.Clear()
+        txtNum3.Clear()
+        txtNum4.Clear()
+        txtSum.Clear()
+        txtPer.Clear()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim num1, num2, num3, num4, sum As Integer
+        Dim percentage As Double
+        'Taking Input values'
+        num1 = txtNum1.Text
+        num2 = txtNum2.Text
+        num3 = txtNum3.Text
+        num4 = txtNum4.Text
+        'Calculating total marks'
+        sum = num1 + num2 + num3 + num4
+        'Calculating the percentage'
+        percentage = (sum / 400) * 100
+        'Displaying the output in textbox'
+        txtSum.Text = sum
+        txtPer.Text = percentage
+
+    End Sub
+End Class
